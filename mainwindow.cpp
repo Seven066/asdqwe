6383d1e1b0e2cfa97f75ca7e00e5243afb0aa5cc
Fourp065 <065@t-sk.ru>
2021-09-26 14:06:47 +0300

    Add base multiplayer functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,31 +1,86 @@
 #include "mainwindow.h"
 
+#include <QDebug>
 #include <QLabel>
 #include <QMenuBar>
+#include <QMessageBox>
+#include <QTimer>
 #include <QVBoxLayout>
 
-#include "gui/chessboard.h"
+const ExitGames::Common::JString id("33cc839c-4ac0-494a-a742-afd1e3bdfec8");
+const ExitGames::Common::JString version("");
 
 namespace Gui {
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
 {
-    Gui::ChessBoard* board = new Gui::ChessBoard();
+    m_board = new Gui::ChessBoard();
     QWidget* dummy = new QWidget();
     QVBoxLayout* mainLayout = new QVBoxLayout(dummy);
     mainLayout->addWidget(new QLabel("Black"));
-    mainLayout->addWidget(board->widget());
+    mainLayout->addWidget(m_board->widget());
     mainLayout->addWidget(new QLabel("White"));
     setCentralWidget(dummy);
     setFixedSize(x(), y());
-
-    board->setfen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    connect(board, &Gui::ChessBoard::positionUpdated, this, [](const QString& fen) {
-        //send position to server
+    m_board->setfen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    connect(m_board, &Gui::ChessBoard::positionUpdated, this, [this](QString fen) {
+        if (m_client->getIsInRoom()) {
+            Common::Hashtable evData;
+            Common::JString str(fen.toStdString().c_str());
+            evData.put((nByte)10, str);
+            m_client->opRaiseEvent(false, evData, 1);
+            qDebug() << "Send event";
+        }
     });
 
     addMenus();
+
+    m_client = new ExitGames::LoadBalancing::Client(*this, id, version);
+    m_client->connect();
+
+    QTimer* timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, [this]() {
+        m_client->service();
+    });
+    timer->start(100);
+
+    m_box = new QMessageBox(QMessageBox::Information, "Chess onine", "Connecting...\nPlease wait!", QMessageBox::Cancel);
+    connect(m_box, &QMessageBox::QDialog::rejected, this, []() {
+        exit(1);
+    });
+    m_box->exec();
+}
+
+void MainWindow::debugReturn(int debugLevel, const Common::JString& string)
+{
+    qDebug() << string;
+}
+
+void MainWindow::joinRoomEventAction(int playerNr, const Common::JVector<int>& playernrs, const LoadBalancing::Player& player)
+{
+    qDebug() << "Player" << playerNr << "joined to the room";
+    m_box->setText("Connected\nJoined to the room");
+    m_box->accept();
+}
+
+void MainWindow::leaveRoomEventAction(int playerNr, bool isInactive)
+{
+    qDebug() << "Player" << playerNr << "left to the room";
+}
+
+void MainWindow::customEventAction(int playerNr, nByte eventCode, const Common::Object& eventContent)
+{
+    ExitGames::Common::Hashtable content = ExitGames::Common::ValueObject<ExitGames::Common::Hashtable>(eventContent).getDataCopy();
+    Common::JString fen = ((Common::ValueObject<Common::JString>*)content.getValue((nByte)10))->getDataCopy();
+    m_board->setfen(std::string(fen.UTF8Representation().cstr()));
+}
+
+void MainWindow::connectReturn(int errorCode, const Common::JString& errorString, const Common::JString& region, const Common::JString& cluster)
+{
+    qDebug() << "Connected";
+    m_box->setText("Connected\nJoin to the room...");
+    m_client->opJoinOrCreateRoom("test");
 }
 
 void MainWindow::addMenus()
@@ -37,4 +92,5 @@ void MainWindow::addMenus()
     m_gameMenu->addSeparator();
     m_gameMenu->addAction("Exit", [this]() { close(); });
 }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,49 @@
 
 #include <QMainWindow>
 
+#include "LoadBalancing-cpp/inc/Client.h"
+#include "gui/chessboard.h" 4
+
+using namespace ExitGames;
 class QMenuBar;
+class QMessageBox;
 
 namespace Gui {
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow, LoadBalancing::Listener {
     Q_OBJECT
 
 public:
     MainWindow(QWidget* parent = nullptr);
+
+    // receive and print out debug out here
+    void debugReturn(int debugLevel, const Common::JString& string);
+
+    // implement your error-handling here
+    void connectionErrorReturn(int errorCode) override { }
+    void clientErrorReturn(int errorCode) override { }
+    void warningReturn(int warningCode) override { }
+    void serverErrorReturn(int errorCode) override { }
+
+    // events, triggered by certain operations of all players in the same room
+    void joinRoomEventAction(int playerNr, const Common::JVector<int>& playernrs, const LoadBalancing::Player& player) override;
+    void leaveRoomEventAction(int playerNr, bool isInactive) override;
+
+    void customEventAction(int playerNr, nByte eventCode, const Common::Object& eventContent) override;
+
+    // callbacks for operations on the server
+    void connectReturn(int errorCode, const Common::JString& errorString, const Common::JString& region, const Common::JString& cluster) override;
+    void disconnectReturn(void) override { }
+    void leaveRoomReturn(int errorCode, const Common::JString& errorString) override { }
 
 private:
     void addMenus();
 signals:
 
 private:
+    ExitGames::LoadBalancing::Client* m_client;
+    Gui::ChessBoard* m_board;
+    QMessageBox* m_box;
     QMenu* m_gameMenu;
     QMenu* m_helpMenu;
 };
